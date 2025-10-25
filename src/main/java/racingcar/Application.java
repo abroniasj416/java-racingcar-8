30cf9227a2cc1f8c28df919a765bf447842606fa
Hwang Soojin <soojin9433@naver.com>
2025-10-26 03:43:19 +0900

    refactor(app): Application 구조 정리 (입출력 분리 확인)

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,7 +2,7 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        new PlayControl().start();
+        new PlayControl().start(); // 입출력 책임이 PlayControl 내부에서 InputView, OutputView로 분리됨
     }
 }
 
