438790fb675920606715af7ac213f35b6e9959af
Hwang Soojin <soojin9433@naver.com>
2025-10-26 01:58:58 +0900

    feat(play): 라운드 상태 출력 및 빈 줄 추가

--- src/main/java/racingcar/Play.java
+++ src/main/java/racingcar/Play.java
@@ -15,6 +15,8 @@ public class Play {
         for (Car car : cars) {
             boolean movable = Randoms.pickNumberInRange(0, 9) >= 4;
             car.moveIf(movable);
+            System.out.println(car.name() + " : " + "-".repeat(car.position()));
         }
+        System.out.println(); // 라운드 종료 후 빈 줄
     }
 }
