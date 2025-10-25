8cbd89c769232e47771ba3f97e680540e5eb7519
Hwang Soojin <soojin9433@naver.com>
2025-10-26 03:42:03 +0900

    refactor(play): Play가 OutputView를 이용해 결과 출력

--- src/main/java/racingcar/Play.java
+++ src/main/java/racingcar/Play.java
@@ -5,18 +5,18 @@ import java.util.List;
 
 public class Play {
     private final List<Car> cars;
+    private final OutputView outputView;
 
-    public Play(List<Car> cars) {
+    public Play(List<Car> cars, OutputView outputView) {
         this.cars = cars;
+        this.outputView = outputView;
     }
 
-    // 라운드 진행만, 콘솔 출력은 다음 커밋에서 처리
     void playOneRound() {
         for (Car car : cars) {
             boolean movable = Randoms.pickNumberInRange(0, 9) >= 4;
             car.moveIf(movable);
-            System.out.println(car.name() + " : " + "-".repeat(car.position()));
         }
-        System.out.println(); // 라운드 종료 후 빈 줄
+        outputView.printRoundResult(cars);
     }
 }
