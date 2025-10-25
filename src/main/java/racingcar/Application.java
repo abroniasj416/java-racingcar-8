d8a2983e7afa4e35c429db279d60ff714ba9377b
Hwang Soojin <soojin9433@naver.com>
2025-10-26 02:56:17 +0900

    feat(app): Application 엔트리포인트 및 예외 처리

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -2,6 +2,11 @@ package racingcar;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        try {
+            new PlayControl().start();
+        } catch (IllegalArgumentException e) {
+            System.out.println(e.getMessage()); // System.exit() 금지
+        }
     }
 }
+
